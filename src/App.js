4832bb6736590552240610fe19f131f05fc10a47
hideto0710 <h.inamura0710@gmail.com>
2016-12-16 16:42:22 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './css/darkroom.css'
+import { Transform } from './Transform';
+
+import { Darkroom, Canvas, Toolbar, FilePicker, CropMenu } from 'react-darkroom';
+
+const createObjectURL = (window.URL || window.webkitURL).createObjectURL || window.createObjectURL;
+const canvasWidth = 480;
+const canvasHeight = 200;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: '',
+      crop: false,
+      save: false,
+    };
+    this.handleChangeFile = this.handleChangeFile.bind(this);
+    this.onCropConfirm = this.onCropConfirm.bind(this);
+    this.onCropStart = this.onCropStart.bind(this);
+    this.onSave = this.onSave.bind(this);
+  }
+
+  handleChangeFile(e) {
+    const files = e.target.files;
+    const image = createObjectURL(files[0]);
+    this.setState({ image: image });
+  }
+
+  onCropConfirm() {
+    const source = this.state.image;
+    const { x, y, width, height } = this.refs.canvasWrapper.cropBox;
+    Transform.cropImage(
+      source,
+      { x, y, width, height },
+      { width: canvasWidth, height: canvasHeight }
+    ).then(image => this.setState({
+      image: image,
+      crop: false,
+    }));
+  }
+
+  onSave() {
+    this.setState({ save: true });
+  }
+
+  onCancel() {
+    this.setState({ save: false });
+  }
+
+  onCropStart() {
+    this.setState({ crop: true })
+  }
+
+  render() {
+    const hasFile = this.state.image !== '';
+
+    return (
+      <div>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to Dash Generator</h2>
+          </div>
+        </div>
+        <Darkroom>
+          <Toolbar>
+            <CropMenu isCropping={this.state.crop}>
+              <button
+                disabled={!hasFile}
+                data-showOnlyWhen='croppingIsOff'
+                onClick={this.onCropStart}
+                data-tipsy="Crop"
+                className="tipsy tipsy--sw">
+                <span className="icon icon-crop"/>
+              </button>
+              <button disabled={!hasFile} data-showOnlyWhen='croppingIsOn' onClick={this.onCropConfirm} style={{color: 'green'}} data-tipsy="Confirm" className="tipsy tipsy--sw">
+                <span className="icon icon-checkmark"/>
+              </button>
+            </CropMenu>
+            <button disabled={!hasFile} onClick={this.onSave} data-tipsy="Save" className="tipsy tipsy--sw">
+              <span className="icon icon-floppy-disk"/>
+            </button>
+            <button
+              disabled={!hasFile}
+              onClick={this.onCancel}
+              style={{color: 'red'}}
+              data-tipsy="Cancel"
+              className="tipsy tipsy--sw">
+              <span className="icon icon-cross"/>
+            </button>
+          </Toolbar>
+          <Canvas ref="canvasWrapper" crop={this.state.crop} source={this.state.image} angle={0} width={canvasWidth} height={canvasHeight}>
+            <FilePicker hasFile={hasFile} onChange={this.handleChangeFile}/>
+          </Canvas>
+        </Darkroom>
+      </div>
+    );
+  }
+}
+
+export default App;
